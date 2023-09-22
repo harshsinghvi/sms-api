da7c0760b706a10b713d9085e153aff833f85abf
Harsh Singhvi <root@harshsinghvi.com>
2023-09-23 02:01:18 +0530

    udpate new function

--- functions/device-crud/src/main.js
+++ functions/device-crud/src/main.js
@@ -3,12 +3,28 @@ import { Client } from 'node-appwrite';
 // This is your Appwrite function
 // It's executed each time we get a request
 export default async ({ req, res, log, error }) => {
-  // Why not try the Appwrite SDK?
-  //
-  // const client = new Client()
-  //    .setEndpoint('https://cloud.appwrite.io/v1')
-  //    .setProject(process.env.APPWRITE_FUNCTION_PROJECT_ID)
-  //    .setKey(process.env.APPWRITE_API_KEY);
+  if (!process.env.APPWRITE_FUNCTION_ENDPOINT || !process.env.APPWRITE_FUNCTION_API_KEY) {
+    error('Environment variables are not set. Function cannot use Appwrite SDK.');
+    return res.send('Environment variables are not set. Function cannot use Appwrite SDK.', 500);
+  }
+
+  const client = new Client()
+    .setEndpoint(process.env.APPWRITE_FUNCTION_ENDPOINT)
+    .setProject(process.env.APPWRITE_FUNCTION_PROJECT_ID)
+    .setKey(process.env.APPWRITE_API_KEY);
+
+  log(client);
+  log(req.bodyRaw); // Raw request body, contains request data
+  log(JSON.stringify(req.body)); // Object from parsed JSON request body, otherwise string
+  log(JSON.stringify(req.headers)); // String key-value pairs of all request headers, keys are lowercase
+  log(req.scheme); // Value of the x-forwarded-proto header, usually http or https
+  log(req.method); // Request method, such as GET, POST, PUT, DELETE, PATCH, etc.
+  log(req.url); // Full URL, for example: http://awesome.appwrite.io:8000/v1/hooks?limit=12&offset=50
+  log(req.host); // Hostname from the host header, such as awesome.appwrite.io
+  log(req.port); // Port from the host header, for example 8000
+  log(req.path); // Path part of URL, for example /v1/hooks
+  log(req.queryString); // Raw query params string. For example "limit=12&offset=50"
+  log(JSON.stringify(req.query));
 
   // You can log messages to the console
   log('Hello, Logs!');
