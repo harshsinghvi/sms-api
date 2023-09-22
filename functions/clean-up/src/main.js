fbb0bb5b5c320a462a72a28a3c7105743add24b9
Harsh Singhvi <root@harshsinghvi.com>
2023-09-23 03:27:15 +0530

    f

--- functions/clean-up/src/main.js
+++ functions/clean-up/src/main.js
@@ -4,7 +4,7 @@ const SMS_DATABSE_ID = 'sms-api';
 const DEVICES_COLLECTION_ID = 'devices';
 
 export default async ({ req, res, log, error }) => {
-  console.log(process.env)
+  console.log(process.env);
   try {
     if (!process.env.APPWRITE_FUNCTION_ENDPOINT || !process.env.APPWRITE_FUNCTION_API_KEY) {
       error('Environment variables are not set. Function cannot use Appwrite SDK.');
@@ -43,8 +43,13 @@ export default async ({ req, res, log, error }) => {
     let promises = [];
 
     for (let id of deleteKeys) {
-      promises.push(database.deleteCollection(SMS_DATABSE_ID, id).then(() => log(id, '=>', 'Collection deleted')));
-      promises.push(database.deleteDocument(SMS_DATABSE_ID, DEVICES_COLLECTION_ID, id).then(() => log(id, '=>', 'Document deleted')));
+      promises.push(database.deleteCollection(SMS_DATABSE_ID, id).then(() => log(id, '=>', 'Collection deleted'))).catch(error);
+      promises.push(
+        database
+          .deleteDocument(SMS_DATABSE_ID, DEVICES_COLLECTION_ID, id)
+          .then(() => log(id, '=>', 'Document deleted'))
+          .catch(error)
+      );
     }
 
     await Promise.all(promises);
