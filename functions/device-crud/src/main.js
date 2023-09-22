57ae69fb4254f2f6c7f2e65afa4a711635422601
Harsh Singhvi <root@harshsinghvi.com>
2023-09-23 02:37:55 +0530

    fix

--- functions/device-crud/src/main.js
+++ functions/device-crud/src/main.js
@@ -16,11 +16,11 @@ export default async ({ req, res, log, error }) => {
 
     const databases = new Databases(client);
 
-    // log(req.bodyRaw); // Raw request body, contains request data
+    log(req.bodyRaw); // Raw request body, contains request data
     // log(JSON.stringify(req.body)); // Object from parsed JSON request body, otherwise string
-    // log(JSON.stringify(req.headers)); // String key-value pairs of all request headers, keys are lowercase
+    log(JSON.stringify(req.headers)); // String key-value pairs of all request headers, keys are lowercase
     // log(req.scheme); // Value of the x-forwarded-proto header, usually http or https
-    // log(req.method); // Request method, such as GET, POST, PUT, DELETE, PATCH, etc.
+    log(req.method); // Request method, such as GET, POST, PUT, DELETE, PATCH, etc.
     // log(req.url); // Full URL, for example: http://awesome.appwrite.io:8000/v1/hooks?limit=12&offset=50
     // log(req.host); // Hostname from the host header, such as awesome.appwrite.io
     // log(req.port); // Port from the host header, for example 8000
